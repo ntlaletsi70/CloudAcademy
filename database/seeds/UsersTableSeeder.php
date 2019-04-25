47a730248a628199453bb86231b1426c9faafad8
Neo Tlaletsi (Santam) <Neo.tlaletsi@santam.co.za>
2019-04-25 09:13:12 +0530

    A few Users Seeded

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,62 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+
+       // factory(App\User::class, 50)->create()->each(function ($user) {
+         //   $user->posts()->save(factory(App\Post::class)->make());
+            
+         $faker = \Faker\Factory::create();
+
+         $password = Hash::make('hello12');
+
+       
+         App\User::create([
+
+            'name' => 'Tony',
+            'email' => 'tonysgoloza@gmail.com',
+            'password'=> $password,
+        ]);
+
+        
+        App\User::create([
+
+            'name' => 'Greg',
+            'email' => 'greg@gmail.com',
+            'password'=> $password,
+        ]);
+
+        
+        App\User::create([
+
+            'name' => 'Kevin',
+            'email' => 'kevin@gmail.com',
+            'password'=> $password,
+        ]);
+
+        
+        App\User::create([
+
+            'name' => 'Mark',
+            'email' => 'mark@gmail.com',
+            'password'=> $password,
+        ]);
+
+        
+        App\User::create([
+
+            'name' => 'Luke',
+            'email' => 'luke@gmail.com',
+            'password'=> $password,
+        ]);
+
+        
+        App\User::create([
+
+            'name' => 'Thabo',
+            'email' => 'thabo@gmail.com',
+            'password'=> $password,
+        ]);
+
+    
     }
 }
